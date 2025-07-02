d86f61a279667c71313155b06740deedec017f9c
Tim-Luca Lagmöller <mail@lagmoellertim.de>
2025-07-02 20:22:05 +0200

    Update essentia_sys

--- essentia_sys/src/lib.rs
+++ essentia_sys/src/lib.rs
@@ -2,73 +2,73 @@
 pub mod ffi {
 
     // ===== Helper Structs =====
-    struct SliceFloat<'a> {
+    pub struct SliceFloat<'a> {
         slice: &'a [f32],
     }
 
-    struct VecString {
+    pub struct VecString {
         vec: Vec<String>,
     }
 
-    struct SliceStereoSample<'a> {
+    pub struct SliceStereoSample<'a> {
         slice: &'a [StereoSample],
     }
 
-    struct MatrixFloat<'a> {
+    pub struct MatrixFloat<'a> {
         slice: &'a [f32],
         dim1: usize,
         dim2: usize,
     }
 
-    struct TensorFloat<'a> {
+    pub struct TensorFloat<'a> {
         slice: &'a [f32],
         shape: &'a [usize],
     }
 
-    struct MapEntryVectorFloat<'a> {
+    pub struct MapEntryVectorFloat<'a> {
         key: String,
         value: &'a [f32],
     }
 
-    struct MapEntryVectorString {
+    pub struct MapEntryVectorString {
         key: String,
         value: Vec<String>,
     }
 
-    struct MapEntryVectorInt<'a> {
+    pub struct MapEntryVectorInt<'a> {
         key: String,
         value: &'a [i32],
     }
 
-    struct MapEntryFloat {
+    pub struct MapEntryFloat {
         key: String,
         value: f32,
     }
 
     #[derive(Clone, Debug)]
-    struct StereoSample {
+    pub struct StereoSample {
         left: f32,
         right: f32,
     }
 
     #[derive(Clone, Debug)]
-    struct Complex {
+    pub struct Complex {
         real: f32,
         imag: f32,
     }
 
-    struct VecComplex {
+    pub struct VecComplex {
         vec: Vec<Complex>,
     }
 
-    struct MapEntryVectorComplex<'a> {
+    pub struct MapEntryVectorComplex<'a> {
         key: String,
         value: &'a [Complex],
     }
 
     // ===== Data Type Enum =====
     #[derive(Debug, Clone, Copy)]
-    enum DataType {
+    pub enum DataType {
         Float,
         String,
         Bool,
@@ -99,7 +99,7 @@ pub mod ffi {
     }
 
     // ===== Introspection Structs =====
-    struct ParameterInfo {
+    pub struct ParameterInfo {
         name: String,
         data_type: DataType,
         constraint: String,
@@ -107,7 +107,7 @@ pub mod ffi {
         default_value: String,
     }
 
-    struct InputOutputInfo {
+    pub struct InputOutputInfo {
         name: String,
         data_type: DataType,
         description: String,
@@ -118,143 +118,152 @@ pub mod ffi {
         include!("bridge/bridge.h");
 
         // ===== Core types =====
-        type AlgorithmBridge;
-        type ParameterMapBridge;
-        type PoolBridge;
-        type VariantData;
+        pub type AlgorithmBridge;
+        pub type ParameterMapBridge;
+        pub type PoolBridge;
+        pub type VariantData;
 
         // ===== Essentia Initialization =====
-        fn init_essentia();
-        fn shutdown_essentia();
+        pub fn init_essentia();
+        pub fn shutdown_essentia();
 
         // ===== Algorithm Bridge Creation =====
-        fn get_algorithm_names() -> Vec<String>;
-        fn create_algorithm_bridge(name: &str) -> Result<UniquePtr<AlgorithmBridge>>;
+        pub fn get_algorithm_names() -> Vec<String>;
+        pub fn create_algorithm_bridge(name: &str) -> Result<UniquePtr<AlgorithmBridge>>;
 
         // ===== Algorithm Bridge Introspection =====
-        fn get_name(self: &AlgorithmBridge) -> String;
-        fn get_category(self: &AlgorithmBridge) -> String;
-        fn get_description(self: &AlgorithmBridge) -> String;
-        fn get_parameter_infos(self: &AlgorithmBridge) -> Vec<ParameterInfo>;
-        fn get_input_infos(self: &AlgorithmBridge) -> Vec<InputOutputInfo>;
-        fn get_output_infos(self: &AlgorithmBridge) -> Vec<InputOutputInfo>;
+        pub fn get_name(self: &AlgorithmBridge) -> String;
+        pub fn get_category(self: &AlgorithmBridge) -> String;
+        pub fn get_description(self: &AlgorithmBridge) -> String;
+        pub fn get_parameter_infos(self: &AlgorithmBridge) -> Vec<ParameterInfo>;
+        pub fn get_input_infos(self: &AlgorithmBridge) -> Vec<InputOutputInfo>;
+        pub fn get_output_infos(self: &AlgorithmBridge) -> Vec<InputOutputInfo>;
 
         // ===== Algorithm Bridge Configuration & Execution =====
-        fn configure(
+        pub fn configure(
             self: Pin<&mut AlgorithmBridge>,
             parameter_map_bridge: UniquePtr<ParameterMapBridge>,
         ) -> Result<()>;
-        fn compute(self: Pin<&mut AlgorithmBridge>) -> Result<()>;
-        fn reset(self: Pin<&mut AlgorithmBridge>) -> Result<()>;
+        pub fn compute(self: Pin<&mut AlgorithmBridge>) -> Result<()>;
+        pub fn reset(self: Pin<&mut AlgorithmBridge>) -> Result<()>;
 
         // ===== Algorithm Bridge Input/Output =====
-        fn set_input(
+        pub fn set_input(
             self: Pin<&mut AlgorithmBridge>,
             input_name: &str,
             variant_data: UniquePtr<VariantData>,
         ) -> Result<()>;
-        fn setup_output(
+        pub fn setup_output(
             self: Pin<&mut AlgorithmBridge>,
             output_name: &str,
             data_type: DataType,
         ) -> Result<()>;
-        fn get_output(self: &AlgorithmBridge, output_name: &str) -> Result<&VariantData>;
+        pub fn get_output(self: &AlgorithmBridge, output_name: &str) -> Result<&VariantData>;
 
         // ===== Variant Data Constructors =====
-        fn create_variant_data_from_bool(value: bool) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_string(value: &str) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_float(value: f32) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_int(value: i32) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_unsigned_int(value: u32) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_long(value: i64) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_stereo_sample(value: StereoSample) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_complex(value: Complex) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_bool(value: &[bool]) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_int(value: &[i32]) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_string(value: &[&str]) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_float(value: &[f32]) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_stereo_sample(
+        pub fn create_variant_data_from_bool(value: bool) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_string(value: &str) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_float(value: f32) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_int(value: i32) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_unsigned_int(value: u32) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_long(value: i64) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_stereo_sample(
+            value: StereoSample,
+        ) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_complex(value: Complex) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_vector_bool(value: &[bool]) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_vector_int(value: &[i32]) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_vector_string(value: &[&str]) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_vector_float(value: &[f32]) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_vector_stereo_sample(
             value: &[StereoSample],
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_complex(value: &[Complex]) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_vector_float(
+        pub fn create_variant_data_from_vector_complex(value: &[Complex])
+        -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_vector_vector_float(
             value: Vec<SliceFloat>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_matrix_float(value: MatrixFloat) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_tensor_float(value: TensorFloat) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_vector_string(
+        pub fn create_variant_data_from_matrix_float(value: MatrixFloat) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_tensor_float(value: TensorFloat) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_vector_vector_string(
             value: Vec<VecString>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_vector_stereo_sample(
+        pub fn create_variant_data_from_vector_vector_stereo_sample(
             value: Vec<SliceStereoSample>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_vector_complex(
+        pub fn create_variant_data_from_vector_vector_complex(
             value: Vec<VecComplex>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_vector_matrix_float(
+        pub fn create_variant_data_from_vector_matrix_float(
             value: Vec<MatrixFloat>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_map_vector_float(
+        pub fn create_variant_data_from_map_vector_float(
             value: Vec<MapEntryVectorFloat>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_map_vector_string(
+        pub fn create_variant_data_from_map_vector_string(
             value: Vec<MapEntryVectorString>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_map_vector_int(
+        pub fn create_variant_data_from_map_vector_int(
             value: Vec<MapEntryVectorInt>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_map_vector_complex(
+        pub fn create_variant_data_from_map_vector_complex(
             value: Vec<MapEntryVectorComplex>,
         ) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_map_float(value: Vec<MapEntryFloat>) -> UniquePtr<VariantData>;
-        fn create_variant_data_from_pool(value: UniquePtr<PoolBridge>) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_map_float(
+            value: Vec<MapEntryFloat>,
+        ) -> UniquePtr<VariantData>;
+        pub fn create_variant_data_from_pool(
+            value: UniquePtr<PoolBridge>,
+        ) -> UniquePtr<VariantData>;
 
         // ===== Variant Data Introspection =====
-        fn get_data_type(self: &VariantData) -> DataType;
+        pub fn get_data_type(self: &VariantData) -> DataType;
 
         // ===== Variant Data Accessors =====
-        fn get_bool(self: &VariantData) -> Result<bool>;
-        fn get_string(self: &VariantData) -> Result<String>;
-        fn get_float(self: &VariantData) -> Result<f32>;
-        fn get_int(self: &VariantData) -> Result<i32>;
-        fn get_unsigned_int(self: &VariantData) -> Result<u32>;
-        fn get_long(self: &VariantData) -> Result<i64>;
-        fn get_stereo_sample(self: &VariantData) -> Result<StereoSample>;
-        fn get_complex(self: &VariantData) -> Result<Complex>;
-        fn get_vector_bool(self: &VariantData) -> Result<Vec<bool>>;
-        fn get_vector_int(self: &VariantData) -> Result<&[i32]>;
-        fn get_vector_string(self: &VariantData) -> Result<Vec<String>>;
-        fn get_vector_float(self: &VariantData) -> Result<&[f32]>;
-        fn get_vector_stereo_sample(self: &VariantData) -> Result<&[StereoSample]>;
-        fn get_vector_complex(self: &VariantData) -> Result<&[Complex]>;
-        fn get_vector_vector_float(self: &VariantData) -> Result<Vec<SliceFloat>>;
-        fn get_matrix_float(self: &VariantData) -> Result<MatrixFloat>;
-        fn get_tensor_float(self: &VariantData) -> Result<TensorFloat>;
-        fn get_vector_vector_string(self: &VariantData) -> Result<Vec<VecString>>;
-        fn get_vector_vector_stereo_sample(self: &VariantData) -> Result<Vec<SliceStereoSample>>;
-        fn get_vector_vector_complex(self: &VariantData) -> Result<Vec<VecComplex>>;
-        fn get_vector_matrix_float(self: &VariantData) -> Result<Vec<MatrixFloat>>;
-        fn get_map_vector_float(self: &VariantData) -> Result<Vec<MapEntryVectorFloat>>;
-        fn get_map_vector_string(self: &VariantData) -> Result<Vec<MapEntryVectorString>>;
-        fn get_map_vector_int(self: &VariantData) -> Result<Vec<MapEntryVectorInt>>;
-        fn get_map_vector_complex(self: &VariantData) -> Result<Vec<MapEntryVectorComplex>>;
-        fn get_map_float(self: &VariantData) -> Result<Vec<MapEntryFloat>>;
-        fn get_pool(self: &VariantData) -> &PoolBridge;
+        pub fn get_bool(self: &VariantData) -> Result<bool>;
+        pub fn get_string(self: &VariantData) -> Result<String>;
+        pub fn get_float(self: &VariantData) -> Result<f32>;
+        pub fn get_int(self: &VariantData) -> Result<i32>;
+        pub fn get_unsigned_int(self: &VariantData) -> Result<u32>;
+        pub fn get_long(self: &VariantData) -> Result<i64>;
+        pub fn get_stereo_sample(self: &VariantData) -> Result<StereoSample>;
+        pub fn get_complex(self: &VariantData) -> Result<Complex>;
+        pub fn get_vector_bool(self: &VariantData) -> Result<Vec<bool>>;
+        pub fn get_vector_int(self: &VariantData) -> Result<&[i32]>;
+        pub fn get_vector_string(self: &VariantData) -> Result<Vec<String>>;
+        pub fn get_vector_float(self: &VariantData) -> Result<&[f32]>;
+        pub fn get_vector_stereo_sample(self: &VariantData) -> Result<&[StereoSample]>;
+        pub fn get_vector_complex(self: &VariantData) -> Result<&[Complex]>;
+        pub fn get_vector_vector_float(self: &VariantData) -> Result<Vec<SliceFloat>>;
+        pub fn get_matrix_float(self: &VariantData) -> Result<MatrixFloat>;
+        pub fn get_tensor_float(self: &VariantData) -> Result<TensorFloat>;
+        pub fn get_vector_vector_string(self: &VariantData) -> Result<Vec<VecString>>;
+        pub fn get_vector_vector_stereo_sample(
+            self: &VariantData,
+        ) -> Result<Vec<SliceStereoSample>>;
+        pub fn get_vector_vector_complex(self: &VariantData) -> Result<Vec<VecComplex>>;
+        pub fn get_vector_matrix_float(self: &VariantData) -> Result<Vec<MatrixFloat>>;
+        pub fn get_map_vector_float(self: &VariantData) -> Result<Vec<MapEntryVectorFloat>>;
+        pub fn get_map_vector_string(self: &VariantData) -> Result<Vec<MapEntryVectorString>>;
+        pub fn get_map_vector_int(self: &VariantData) -> Result<Vec<MapEntryVectorInt>>;
+        pub fn get_map_vector_complex(self: &VariantData) -> Result<Vec<MapEntryVectorComplex>>;
+        pub fn get_map_float(self: &VariantData) -> Result<Vec<MapEntryFloat>>;
+        pub fn get_pool(self: &VariantData) -> &PoolBridge;
 
         // ===== Parameter Map Bridge =====
-        fn create_parameter_map_bridge() -> UniquePtr<ParameterMapBridge>;
-        fn add(
+        pub fn create_parameter_map_bridge() -> UniquePtr<ParameterMapBridge>;
+        pub fn add(
             self: Pin<&mut ParameterMapBridge>,
             key: &str,
             variant_data: UniquePtr<VariantData>,
         ) -> Result<()>;
 
         // ===== Pool Bridge =====
-        fn create_pool_bridge() -> UniquePtr<PoolBridge>;
-        fn clone(self: &PoolBridge) -> UniquePtr<PoolBridge>;
-        fn set(self: Pin<&mut PoolBridge>, key: &str, variant_data: UniquePtr<VariantData>);
-        fn get(self: &PoolBridge, key: &str) -> Result<UniquePtr<VariantData>>;
-        fn contains(self: &PoolBridge, key: &str) -> bool;
-        fn keys(self: &PoolBridge) -> Vec<String>;
+        pub fn create_pool_bridge() -> UniquePtr<PoolBridge>;
+        pub fn clone(self: &PoolBridge) -> UniquePtr<PoolBridge>;
+        pub fn set(self: Pin<&mut PoolBridge>, key: &str, variant_data: UniquePtr<VariantData>);
+        pub fn get(self: &PoolBridge, key: &str) -> Result<UniquePtr<VariantData>>;
+        pub fn contains(self: &PoolBridge, key: &str) -> bool;
+        pub fn keys(self: &PoolBridge) -> Vec<String>;
     }
 }
