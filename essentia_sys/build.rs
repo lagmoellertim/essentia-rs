cb8b178eef93104a827b68b1da40ff51febfbed1
Tim-Luca Lagmöller <mail@lagmoellertim.de>
2025-07-03 14:37:47 +0200

    Update build

--- essentia_sys/build.rs
+++ essentia_sys/build.rs
@@ -1,5 +1,22 @@
+struct Library {
+    pub name: String,
+    pub pkg_config_name: String,
+    pub link_name: Option<String>,
+}
+
+impl Library {
+    fn new(name: &str, pkg_config_name: &str, link_name: Option<&str>) -> Self {
+        Self {
+            name: name.to_string(),
+            pkg_config_name: pkg_config_name.to_string(),
+            link_name: link_name.map(|name| name.to_string()),
+        }
+    }
+}
+
 fn main() {
-    cxx_build::bridge("src/lib.rs")
+    let mut build = cxx_build::bridge("src/lib.rs");
+    build
         .file("bridge/bridge.cpp")
         .file("bridge/algorithm_bridge/core.cpp")
         .file("bridge/algorithm_bridge/input_output.cpp")
@@ -10,42 +27,50 @@ fn main() {
         .file("bridge/data_container/constructors.cpp")
         .file("bridge/data_container/introspection.cpp")
         .file("bridge/common/type_mapping.cpp")
-        .include("target") // Include target directory so essentia/bridge/bridge.h can be found
-        .include("bridge") // Add bridge directory to include path
-        .include(".") // Add project root to include path for bridge/bridge.h
-        .include("/usr/local/include")
-        .include("/opt/homebrew/include")
-        .include("/opt/homebrew/include/eigen3")
-        .include("/opt/homebrew/opt/ffmpeg@4/include")
-        .flag("-std=c++17")
-        .flag("-DESSENTIA_VERSION=2")
-        .flag("-DNO_TENSORFLOW") // Disable TensorFlow features
-        .compile("essentia-bridge");
+        .include(".");
 
-    println!("cargo:rustc-link-lib=essentia");
-    println!("cargo:rustc-link-lib=yaml");
-    println!("cargo:rustc-link-lib=fftw3f");
-    println!("cargo:rustc-link-lib=tag");
-    println!("cargo:rustc-link-lib=samplerate");
-    println!("cargo:rustc-link-lib=tensorflow");
-    println!("cargo:rustc-link-lib=chromaprint");
+    let libraries = [
+        Library::new("essentia", "essentia", Some("essentia")),
+        Library::new("eigen3", "eigen3", None),
+        Library::new("yaml", "yaml-0.1", Some("yaml")),
+        Library::new("fftw3f", "fftw3f", Some("fftw3f")),
+        Library::new("taglib", "taglib", Some("tag")),
+        Library::new("samplerate", "samplerate", Some("samplerate")),
+        Library::new("chromaprint", "libchromaprint", Some("chromaprint")),
+        Library::new("avformat", "libavformat", Some("avformat")),
+        Library::new("swresample", "libswresample", Some("swresample")),
+        Library::new("avcodec", "libavcodec", Some("avcodec")),
+        Library::new("avutil", "libavutil", Some("avutil")),
+        Library::new("tensorflow", "tensorflow", Some("tensorflow")),
+    ];
 
-    // Try linking to FFmpeg libraries at older deployment target
-    println!("cargo:rustc-link-lib=avformat");
-    println!("cargo:rustc-link-lib=avcodec");
-    println!("cargo:rustc-link-lib=avutil");
-    println!("cargo:rustc-link-lib=swresample");
+    for library in libraries {
+        let pkg_info =
+            pkg_config::probe_library(&library.pkg_config_name).expect("Failed to probe library");
 
-    // Link to frameworks on macOS
-    #[cfg(target_os = "macos")]
-    {
-        println!("cargo:rustc-link-lib=framework=Accelerate");
-        println!("cargo:rustc-link-lib=framework=CoreFoundation");
+        println!("{:?}", pkg_info);
+
+        for mut include_path in pkg_info.include_paths {
+            if library.name == "eigen3" {
+                include_path.push("eigen3");
+            }
+
+            build.include(include_path);
+        }
+
+        for link_path in &pkg_info.link_paths {
+            println!(
+                "cargo:rustc-link-search=native={}",
+                link_path.to_string_lossy()
+            );
+        }
+
+        if let Some(link_name) = &library.link_name {
+            println!("cargo:rustc-link-lib={}", link_name);
+        }
     }
 
-    println!("cargo:rustc-link-search=native=/opt/homebrew/opt/ffmpeg@4/lib");
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
-    println!("cargo:rustc-link-search=native=/opt/homebrew/lib");
+    build.flag("-std=c++17").compile("essentia-bridge");
 
     println!("cargo:rerun-if-changed=bridge");
     println!("cargo:rerun-if-changed=src/lib.rs");
